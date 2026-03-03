9a3df4246d16f97a80e86014d0318fece4f52edd
AkitoYamashita <AkitoYamashita@users.noreply.github.com>
2026-03-03 14:34:39 +0900

    Refactor output messages in setup-win.ps1

--- setup-win.ps1
+++ setup-win.ps1
@@ -7,17 +7,16 @@ $Url = "https://github.com/sagiegurari/cargo-make/releases/download/$Version/car
 $Zip = "cargo-make.zip"
 $Dir = "cargo-make"
 
-# zip なければ DL
 if (-not (Test-Path $Zip)) {
 	Write-Host "download..."
 	Invoke-WebRequest -Uri $Url -OutFile $Zip
 }
 
-# 解凍ディレクトリなければ 解凍
 if (-not (Test-Path $Dir)) {
 	Write-Host "extract..."
 	Expand-Archive -Path $Zip -DestinationPath $Dir
 }
 
 Write-Host "done."
-Write-Host "binary: .\$Dir\cargo-make-v$Version-x86_64-pc-windows-msvc\cargo-make.exe"
+Write-Host "binary:"
+Write-Host ".\$Dir\cargo-make-v$Version-x86_64-pc-windows-msvc\cargo-make.exe"
